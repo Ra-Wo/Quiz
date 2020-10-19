fdb44d1ff6849394347a1f45a9d7deee201e77ce
Rasheed Wodosh <36020015+Ra-Wo@users.noreply.github.com>
2020-10-19 22:59:29 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,33 @@
+var questions = [ 
+    
+    {
+        qst: 'من كم لون يتكون قوس قزح؟',
+        answerA: '3 ألوان',
+        answerB: '8 ألوان',
+        answerC: '7 ألوان',
+        trueAnswer: '7 ألوان'
+    },
+    
+    {
+        qst: 'كم عدد أحرف اللغة الإنجليزية؟',
+        answerA: '22 حرف',
+        answerB: '26 حرف',
+        answerC: '16 حرف',
+        trueAnswer: '26 حرف'
+        
+    },
+    
+    {
+        qst: 'ما هي الدولة التي اشتهرت بالسوشي؟',
+        answerA: 'فرنسا',
+        answerB: 'اليابان',
+        answerC: 'لبنان',
+        trueAnswer: 'اليابان'
+        
+    },
+    
+    
+];
+    
+var numQst = questions.length;
+
